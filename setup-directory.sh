315b290772bab7af0dc011b1f7864fcf69cbf3ce
Duc Tran <thd1152015@gmail.com>
2021-08-09 18:09:28 +0700

    initial

---
+++ setup-directory.sh
@@ -0,0 +1,10 @@
+mkdir -p ~/.config/nvim
+mkdir -p ~/.config/nvim/after/plugin
+mkdir -p ~/.config/nvim/colors
+
+cd ~/.config/nvim
+
+echo '"Init NVIM' > init.vim
+echo '"Macos configuration' > macos.vim
+echo '"Key mapping NVIM' > maps.vim
+echo '"Plugin configuration' > plug.vim

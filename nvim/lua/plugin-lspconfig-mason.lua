deae78d836601f4c0908d0c81ee5cd147c090a7b
Duc Tran <thd1152015@gmail.com>
2023-05-05 10:58:37 +0700

    latest

--- nvim/lua/plugin-lspconfig-mason.lua
+++ nvim/lua/plugin-lspconfig-mason.lua
@@ -1,11 +1,22 @@
 require("mason").setup({
-    ui = {
-        icons = {
-            package_installed = "✓",
-            package_pending = "➜",
-            package_uninstalled = "✗"
-        }
-    }
+  ui = {
+      icons = {
+          package_installed = "✓",
+          package_pending = "➜",
+          package_uninstalled = "✗"
+      }
+  }
 })
+
+require("mason-lspconfig").setup({
+  ensure_installed = { "lua_ls", "rust_analyzer", "terraformls", "tsserver", "vimls", "yamlls" }
+})
+
+require("lspconfig").lua_ls.setup {}
+require("lspconfig").rust_analyzer.setup {}
+require("lspconfig").terraformls.setup {}
+require("lspconfig").tsserver.setup {}
+require("lspconfig").vimls.setup {}
+require("lspconfig").yamlls.setup {}
 
 return

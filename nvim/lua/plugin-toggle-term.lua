18ea433e908763d318c74fc5e1e7cf20d50abed4
Duc Tran <thd1152015@gmail.com>
2023-05-05 09:29:01 +0700

    latest

--- nvim/lua/plugin-toggle-term.lua
+++ nvim/lua/plugin-toggle-term.lua
@@ -2,9 +2,9 @@ require("toggleterm").setup{
   -- size can be a number or function which is passed the current terminal
   size = function(term)
     if term.direction == "horizontal" then
-      return 15
+      return 30
     elseif term.direction == "vertical" then
-      return vim.o.columns * 0.4
+      return vim.o.columns * 0.6
     end
   end
 }

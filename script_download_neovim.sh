8549e3a432436d599b6824463afc0a8662aafd07
Duc <thd1152015@gmail.com>
2024-02-29 10:45:51 +0700

    latest

--- script_download_neovim.sh
+++ script_download_neovim.sh
@@ -4,6 +4,6 @@ echo 'download and install neovim' $VERSION
 curl -L https://github.com/neovim/neovim/releases/download/$VERSION/nvim-linux64.tar.gz > nvim-linux64.tar.gz
 tar xzvf nvim-linux64.tar.gz
 mv nvim-linux64 /usr/bin/
-# echo 'export PATH=$PATH/usr/bin/nvim-linux64/bin/nvim' > ~/.profile
+# echo 'export PATH=$PATH:/usr/bin/nvim-linux64/bin' >> ~/.profile
 # source ~/.profile
 # sudo dpkg -i --force-overwrite ./nvim-linux64.deb

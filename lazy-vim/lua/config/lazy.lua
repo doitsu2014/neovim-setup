3a8e95d354ccb5dc30236fb4ed683cfee6d720eb
ドイツ <thd1152015@gmail.com>
2024-03-04 22:33:03 +0700

    Update

---
+++ lazy-vim/lua/config/lazy.lua
@@ -0,0 +1,23 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  -- bootstrap lazy.nvim
+  -- stylua: ignore
+  vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
+end
+vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
+
+require("lazy").setup({
+  spec = {
+    { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+    { import = "lazyvim.plugins.extras.coding.copilot" },
+    { import = "lazyvim.plugins.extras.lang.rust" },
+    { import = "lazyvim.plugins.extras.lang.tailwind" },
+    { import = "lazyvim.plugins.extras.lang.terraform" },
+    { import = "lazyvim.plugins.extras.lang.typescript" },
+    { import = "lazyvim.plugins.extras.lang.yaml" },
+    { import = "lazyvim.plugins.extras.lang.docker" },
+    { import = "lazyvim.plugins.extras.formatting.prettier" },
+    { import = "lazyvim.plugins.extras.linting.eslint" },
+    { import = "plugins" },
+  },
+})
--- script_setup_lazy_neovim_plugins.sh
+++ script_setup_lazy_neovim_plugins.sh
@@ -1,1 +1,2 @@
+cp -R ./lazy-vim/lua/config/* ~/.config/nvim/lua/config/
 cp -R ./lazy-vim/lua/plugins/* ~/.config/nvim/lua/plugins/

1a1c65a1a46e7741ebaa5271626953266ea3c693
ドイツ <thd1152015@gmail.com>
2024-03-04 22:44:28 +0700

    Latest LazyVim

--- script_download_neovim.sh
+++ script_download_neovim.sh
@@ -1,9 +1,8 @@
 VERSION=v0.9.5
 mkdir -p download
 echo 'download and install neovim' $VERSION
-curl -L https://github.com/neovim/neovim/releases/download/$VERSION/nvim-linux64.tar.gz > nvim-linux64.tar.gz
+curl -L https://github.com/neovim/neovim/releases/download/$VERSION/nvim-linux64.tar.gz >nvim-linux64.tar.gz
 tar xzvf nvim-linux64.tar.gz
 mv nvim-linux64 /usr/bin/
-# echo 'export PATH=$PATH:/usr/bin/nvim-linux64/bin' >> ~/.profile
-# source ~/.profile
-# sudo dpkg -i --force-overwrite ./nvim-linux64.deb
+echo 'export PATH=$PATH:/usr/bin/nvim-linux64/bin' >>~/.profile
+echo 'export PATH=$PATH:/usr/bin/nvim-linux64/bin' >>~/.zshrc

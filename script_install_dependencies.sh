7163f857081727e284d1848e1af9a9b8f4e9cd1b
Duc <me@ducth.dev>
2025-02-06 06:34:23 +0000

    latest

--- script_install_dependencies.sh
+++ script_install_dependencies.sh
@@ -17,13 +17,11 @@ elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
   # this if statement is used to install dependency packages of ubuntu which has the version > 16.x.x
   sudo apt update
   sudo apt-get install libtool autoconf automake cmake libncurses5-dev g++ build-essential -y
-  sudo apt-get install curl -y
-  sudo apt-get install git -y
-  sudo apt-get install nodejs npm -y
   sudo apt-get install zip unzip -y
   sudo apt-get install python-dev python-pip python3-dev python3-pip
   sudo apt-get install apt-get install ripgrep fd-find -y
   sudo apt-get install luajit -y
+  sudo apt-get install xclip -y
 fi
 
 sudo bash ./script_download_neovim.sh

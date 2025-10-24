61667bf2d31c81fae49c22e8182fa2eaaf4e288d
Duc <thd1152015@gmail.com>
2025-10-24 09:42:24 +0700

    latest

--- script_download_neovim.sh
+++ script_download_neovim.sh
@@ -1,4 +1,4 @@
-VERSION=v0.11.2
+VERSION=v0.11.4
 
 NVIM_TAG=nvim-linux-x86_64.tar.gz
 NVIM_UNZIP_NAME=nvim-linux-x86_64
@@ -9,21 +9,27 @@ if [ "$(uname)" == "Darwin" ]; then
 fi
 
 echo "create folder downloads $HOME/downloads"
-mkdir -p $HOME/downloads
+mkdir -p "$HOME"/downloads
 echo "download and install neovim" $VERSION
-curl -L https://github.com/neovim/neovim/releases/download/$VERSION/$NVIM_TAG >$HOME/downloads/$NVIM_TAG
+curl -L https://github.com/neovim/neovim/releases/download/$VERSION/$NVIM_TAG >"$HOME"/downloads/$NVIM_TAG
 
 echo "Change directory to $HOME/downloads"
-cd $HOME/downloads
+cd "$HOME"/downloads || exit
 
 echo "Extract $NVIM_TAG"
 tar xzf $NVIM_TAG
 # tar xzvf $NVIM_TAG # verbosely
 
 echo "Remove if $HOME/$NVIM_UNZIP_NAME does exist"
-rm -rf $HOME/$NVIM_UNZIP_NAME
+rm -rf "$HOME"/$NVIM_UNZIP_NAME
 
-mv $NVIM_UNZIP_NAME $HOME/
+mv $NVIM_UNZIP_NAME "$HOME/"
+
 echo "Installed nvim"
-echo 'export PATH=$PATH'":$HOME/$NVIM_UNZIP_NAME/bin" >>~/.profile
-echo 'export PATH=$PATH'":$HOME/$NVIM_UNZIP_NAME/bin" >>~/.zshrc
+
+if ! grep -q "$HOME/$NVIM_UNZIP_NAME/bin" ~/.profile; then
+  echo "export PATH=$PATH:$HOME/$NVIM_UNZIP_NAME/bin" >>~/.profile
+fi
+if ! grep -q "$HOME/$NVIM_UNZIP_NAME/bin" ~/.zshrc; then
+  echo "export PATH=$PATH:$HOME/$NVIM_UNZIP_NAME/bin" >>~/.zshrc
+fi
